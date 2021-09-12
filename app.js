1fc62d2e79fd00b5495032f3de3a5eeaae743731
Cai <trieungochai.dev@gmail.com>
2021-09-12 23:24:28 +0700

    Using Body Parser to Parse POST Requests to the Server

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1d47f431cd5781e0016399043e86fa8f&units=metric";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  console.log(query);
+  const apiKey = "1d47f431cd5781e0016399043e86fa8f";
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
 
   https.get(url, function (response) {
     response.on("data", function (data) {
@@ -16,11 +25,13 @@ app.get("/", function (req, res) {
       const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
       res.write(`<p>The weather is currently ${weatherDescription}</p>`);
-      res.write(`<h1>The temperature in London is ${temp}°C</h1>`);
+      res.write(`<h1>The temperature in ${query} is ${temp}°C</h1>`);
       res.write(`<img src=${imageURL}>`);
       res.send();
     });
   });
+
+  console.log("Post request received.");
 });
 
 app.listen(3000, function () {
